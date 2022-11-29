e1f1648359313b642b63d53248ed7ee9e4b75004
vandersonmauricio <vandermau678@gmail.com>
2022-11-29 10:30:46 -0300

    estudos finalizados

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,12 +1,13 @@
 import { FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa' 
 
+
 import style from './Footer.module.css'
 
 function Footer(){
     return (
         <footer>
             <ul className={style.social_list}>
-            <li><FaFacebook/></li>
+            <a href="https://github.com/vandersonmauricio" target="_blank" rel="noopener noreferrer"><li><FaFacebook/></li> </a> 
             <li><FaInstagram/></li>
             <li><FaLinkedin/></li>
             </ul>
